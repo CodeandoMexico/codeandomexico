cccfaa963e57bcc22e658c70d82ed40f3c79413d
alexfuser <alexfuser@mozilla-mexico.org>
2018-05-23 12:48:09 -0500

    New Files

---
+++ web/estudiantes.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="refresh" content="0; url=https://web-codeando-estudiantes.herokuapp.com/" />
+</head>
+<body>
+
+</body>
+</html>
