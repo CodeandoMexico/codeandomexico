29bb3ab5f24ef182befd81ece4a7ed284a90cc32
alexfuser <alexfuser@mozilla-mexico.org>
2019-12-11 13:06:55 -0600

    historias

--- web/aulascmx/home.php
+++ web/aulascmx/home.php
@@ -355,33 +355,33 @@
 		<div class="overlay"></div>
 		<div class="container">
 			<div class="section-title">
-				<h2>Get in <span>the Lab</span> and  meet the team</h2>
+				<h2>Conoce nuestras <span>historias</span> en Aulas</h2>
 			</div>
 			<div class="row">
 				<!-- single member  -->
-				<div class="col-sm-4">
+				<a><div class="col-sm-6">
 					<div class="member">
 						<img src="img/team/1.jpg" alt="">
-						<h2>Christinne Williams</h2>
-						<h3>Project Manager</h3>
-					</div>
-				</div>
-				<!-- single member  -->
-				<div class="col-sm-4">
-					<div class="member">
-						<img src="img/team/2.jpg" alt="">
-						<h2>Christinne Williams</h2>
+						<h2>Rosa Ortega</h2>
 						<h3>Junior developer</h3>
 					</div>
-				</div>
-				<!-- single member -->
+				</div></a>
+				<!-- single member  -->
+				<a><div class="col-sm-6">
+					<div class="member">
+						<img src="img/team/2.jpg" alt="">
+						<h2>Ricardo Zúñiga</h2>
+						<h3>Junior developer</h3>
+					</div>
+				</div></a>
+				<!-- single member 
 				<div class="col-sm-4">
 					<div class="member">
 						<img src="img/team/3.jpg" alt="">
 						<h2>Christinne Williams</h2>
 						<h3>Digital designer</h3>
 					</div>
-				</div>
+				</div>-->
 			</div>
 		</div>
 	</div>
